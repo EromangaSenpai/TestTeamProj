88935538095a4d0bd65e3668a79c628a8815cf4c
fateev <fateev@101-01>
2018-08-28 20:06:51 +0300

    Add project files.

---
+++ TestTeamProj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTeamProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
